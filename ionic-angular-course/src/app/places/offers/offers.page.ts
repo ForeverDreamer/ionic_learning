6fdcb1cef109ce4d984e293ba0a658b13cd3014d
doer <499361328@qq.com>
2019-07-15 20:41:31 +0800

    ionic-angular-course(08 Ionic Components Overview) 129) Swipeable List Items

--- ionic-angular-course/src/app/places/offers/offers.page.ts
+++ ionic-angular-course/src/app/places/offers/offers.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { IonItemSliding } from '@ionic/angular';
+
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+
 
 @Component({
   selector: 'app-offers',
@@ -11,10 +16,16 @@ export class OffersPage implements OnInit {
 
   offers: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
     this.offers = this.placesService.places;
+  }
+
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+    console.log('Editing item', offerId);
   }
 
 }
