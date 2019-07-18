39546201ab2c4f20bb3dc30239ff4637291b6724
doer <499361328@qq.com>
2019-07-18 11:04:15 +0800

    ionic-angular-course(11 Managing State) 182) UI State with Bookable Places

--- ionic-angular-course/src/app/places/discover/discover.page.ts
+++ ionic-angular-course/src/app/places/discover/discover.page.ts
@@ -5,6 +5,7 @@ import { MenuController } from '@ionic/angular';
 
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import {AuthService} from '../../auth/auth.service';
 
 
 
@@ -16,15 +17,21 @@ import { Place } from '../place.model';
 export class DiscoverPage implements OnInit, OnDestroy {
   loadedPlaces: Place[];
   listedLoadedPlaces: Place[];
+  relevantPlaces: Place[];
   isLoading = false;
   private placesSub: Subscription;
 
-  constructor(private placesService: PlacesService, private menuCtrl: MenuController) { }
+  constructor(
+      private placesService: PlacesService,
+      private menuCtrl: MenuController,
+      private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.placesSub = this.placesService.places.subscribe(places => {
       this.loadedPlaces = places;
-      this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+      this.relevantPlaces = this.loadedPlaces;
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
     });
   }
 
@@ -33,7 +40,14 @@ export class DiscoverPage implements OnInit, OnDestroy {
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
-    console.log(event.detail);
+    if (event.detail.value === 'all') {
+      this.relevantPlaces = this.loadedPlaces;
+    } else {
+      this.relevantPlaces = this.loadedPlaces.filter(
+          place => place.userId !== this.authService.userId
+      );
+    }
+    this.listedLoadedPlaces = this.relevantPlaces.slice(1);
   }
 
   ngOnDestroy() {
